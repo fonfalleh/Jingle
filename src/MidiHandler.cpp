69646e0b66dfd0f5871d8c18c061a6621e72d346
Jacob <jacob.rippe@gmail.com>
2016-04-09 22:25:55 +0200

    Continued MIDI-tests. Have basic structure of Midi-files now. Will try to do timed events according to midi-timestamps.

--- src/MidiHandler.cpp
+++ src/MidiHandler.cpp
@@ -6,9 +6,23 @@ MidiHandler::MidiHandler(const string& filename):
     if (midifile.status()){
         std::cout<<"Midi OK!"<<std::endl;
         std::cout<<midifile.getEventCount(0)<<std::endl;
-        midifile.joinTracks();
+        //midifile.joinTracks();
         std::cout<<midifile.getEventCount(0)<<std::endl;
         
+        int high = 0, low = 100;
+        int track = 1;
+        for (int i=0; i<midifile[track].size(); i++) {
+            if (!midifile[track][i].isNoteOn()) {
+                continue;
+            }
+            int tmp = (int) midifile[track][i][1];
+            if(tmp > high) high = tmp;
+            if(tmp < low) low = tmp;
+            std::cout << midifile[track][i].getDurationInSeconds() 
+                << '\t' << tmp << '\t' << midifile[track][i].tick
+                << std::endl;
+        }
+        std::cout << "high: " << high << "\t low: " << low << std::endl; 
     }
 }
 
