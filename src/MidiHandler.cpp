8272dba90f1c0a7b7f7a8b7dfa1f41ac518efd50
Jacob <jacob.rippe@gmail.com>
2016-04-09 22:50:03 +0200

    Some TODOs and comments

--- src/MidiHandler.cpp
+++ src/MidiHandler.cpp
@@ -9,17 +9,17 @@ MidiHandler::MidiHandler(const string& filename):
         //midifile.joinTracks();
         std::cout<<midifile.getEventCount(0)<<std::endl;
         
-        int high = 0, low = 100;
+        int high = 0, low = 100; // Keeping track of the range of the midi sequence. Ugly magic numbers atm.
         int track = 1;
         for (int i=0; i<midifile[track].size(); i++) {
-            if (!midifile[track][i].isNoteOn()) {
+            if (!midifile[track][i].isNoteOn()) { // Only check messages that are "note-ons"
                 continue;
             }
-            int tmp = (int) midifile[track][i][1];
+            int tmp = (int) midifile[track][i][1]; //current note
             if(tmp > high) high = tmp;
             if(tmp < low) low = tmp;
-            std::cout << midifile[track][i].getDurationInSeconds() 
-                << '\t' << tmp << '\t' << midifile[track][i].tick
+            std::cout << midifile[track][i].getDurationInSeconds() // Not very useful in this case, but can be made interesting.
+                << '\t' << tmp << '\t' << midifile.getTimeInSeconds(track, i) // To be adjusted to make use of frames.
                 << std::endl;
         }
         std::cout << "high: " << high << "\t low: " << low << std::endl; 
