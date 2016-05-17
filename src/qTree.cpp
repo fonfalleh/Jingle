18164229a88f0a71edde0a245082497bf098d068
fonfalleh <jacob.rippe@gmail.com>
2016-05-17 08:03:33 +0200

    Modifiyng a little, planning a little. Hopefully fixed some untested logic. Remains to be tested.

--- src/qTree.cpp
+++ src/qTree.cpp
@@ -1,20 +1,35 @@
 #include "qTree.h"
 
-qTree::qTree(AABB & border, int limit):
-area{border}, center{Point{border.se.x / 2, border.se.y / 2}}, oblimit{limit}
+qTree::qTree(AABB & border, int limit, qTree* root):
+area{border}, center{Point{border.se.x / 2, border.se.y / 2}}, oblimit{limit},
+    nwc{nullptr}, nec{nullptr}, swc{nullptr}, sec{nullptr}, treeroot{this}, isLeaf{true}
 {
+        //TODO: take some list of objects to create in?
+        // OR: don't have insertion as  part of creation, but handled iteratively
 }
 
 qTree::~qTree()
 {
 }
+void qTree::subdivide()
+{
+    //TODO
+    isLeaf = false;
+    
+    //take all objects, split them by region
+    //create subtrees, insert objects into new trees
+    
+}
 
 bool AABB::contains(const Point& p)
-{
+{ // Not tested
     return (this->se.x - p.x >= this->nw.x && this->se.y - p.y >= this->nw.y);
 }
 
 bool AABB::overlaps(AABB & that)
-{
-    return contains(that.nw) || contains(that.se);
+{ // Not tested
+    if((that.nw.x >= nw.x && that.nw.x <= se.x) || (that.se.x <= se.x && that.se.x >= nw.x)){ // horizontal check
+        return (that.nw.y >= nw.y && that.nw.y <= nw.y) || (that.se.y <= se.y && that.se.y >= nw.y); //vertical check
+    }
+    return false;
 }
--- src/qTree.h
+++ src/qTree.h
@@ -1,5 +1,21 @@
 #ifndef QTREE_H
 #define QTREE_H
+
+#include <vector>
+/*
+ * TODO:
+ * Perhaps static lists of all objects and/or leaf nodes
+ * 
+ * Manipulation logic and implementation
+ *      also decide where to handle, and when to update. Don't rebuild entire tree all the time.
+ *      also, multiple trees? Only objects in trees? Only check certain types of objects? Only insert certain types of objects?
+ * 
+ * Using templates? Or handle game-logic someplace... sync coords etc
+ * 
+ * Update-functions
+ *      update coordinates
+ *      move objects between nodes
+ * */
 struct Point
 {
 public:
@@ -15,20 +31,24 @@ public:
     nw{snw}, se{sse} {}
     bool contains(const Point &);
     bool overlaps(AABB &);
-    const Point nw, se;
-    
+    const Point nw, se;    
 };
 
 class qTree
 {
 public:
-    qTree(AABB &, int);
+    // Args: AABB area, object limit, and root node
+    qTree(AABB &, int, qTree*);
     ~qTree();
     AABB area;
     Point center;
     int oblimit;
+    //std::vector<class A> list;
 private:
+    bool isLeaf;
+    void subdivide();
     qTree* nwc, * nec,* swc,* sec;
+    qTree* treeroot;
 };
 
 
